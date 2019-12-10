7099fc80fc967b3a61969dd94447d1b42385142b
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-10 19:09:19 +0530

    variable

---
+++ Test2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x=10;
+	string y="rajitha";
+	char z='b';
+	bool b=true;
+	double a=2.3342;
+	cout<<x<<y<<endl;
+	cout<<y<<endl;
+	cout<<z<<endl;
+	cout<<b<<endl; 
+	cout<<a+x<<endl; 
+}
