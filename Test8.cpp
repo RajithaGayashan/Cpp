ed5b692f8138ba53f61f7527187d1085626d4e99
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-17 09:31:12 +0530

    Floating point variable types

---
+++ Test8.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	float fValue=8768.876786;
+	cout<<fValue<<endl;
+	cout<<sizeof(float)<<endl;
+	
+	double dValue= 65675.5647467;
+	cout<<dValue<<endl;
+	cout<<sizeof(double)<<endl;
+}
