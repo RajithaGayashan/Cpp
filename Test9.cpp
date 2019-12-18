60020f6fd2f8f523e072d5e6e94f5b51e7485d06
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-18 22:35:36 +0530

    Char and bool

---
+++ Test9.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	bool b1=98;
+	bool b2=true;
+	cout<<b1<<endl;
+	cout<<b2<<endl;
+	b2=false;
+	cout<<b2<<endl;
+	b2=657;
+	cout<<b2<<endl;
+	
+	char c1='7';	
+	char c2=7;
+	cout<<c1<<endl;
+	cout<<c2<<endl;
+	cout<<sizeof(char)<<endl;
+	cout<<sizeof(bool)<<endl;
+		
+}
