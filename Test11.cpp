9a87b2e735660fa4f552ceebcfbfb486281bf311
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-20 00:26:00 +0530

    While loop

---
+++ Test11.cpp
@@ -0,0 +1,13 @@
+//while loop
+#include<iostream>
+using namespace std;
+int main(){
+	bool success=false;
+	while(success!=true){
+		cout<<"Keep Moving Forward"<<endl;;
+		success=true;
+		success=90;
+	}
+	cout<<"Your are success person";
+	
+}
