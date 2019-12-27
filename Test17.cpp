b5294fad8cee3863b2d51be0b5e86930ffb2e0db
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-27 22:26:13 +0530

    break -(2)

---
+++ Test17.cpp
@@ -0,0 +1,21 @@
+//break -(2)
+#include<iostream>
+using namespace std;
+int main(){
+	const string password="rajitha123";
+	string input;
+	do{
+		cout<<"Enter the password : ";
+		cin>>input;
+		if(input==password){
+			cout<<"Password accepted"<<endl;
+			break;
+		}
+		else{
+			cout<<"Invalid password"<<endl;
+		}
+	}
+	while(true);
+	cout<<"End"<<endl;
+		
+}
