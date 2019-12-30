ad1bdf3a68c20e47217bfd2f8da03b9631bde4e7
RajithaGayashan <rajithagayashan95@gmail.com>
2019-12-30 10:28:25 +0530

    Size of array

---
+++ Test19.cpp
@@ -0,0 +1,14 @@
+//Size of array
+#include<iostream>
+using namespace std;
+int main(){
+	int value[10]={1,2,3,4,5};
+	for(int i=0;i<=sizeof(value)/sizeof(int);i++){
+		cout<<value[i]<<endl;
+	}
+	cout<<"..............."<<endl;
+	cout<<sizeof(int)<<endl;
+	cout<<sizeof(value)<<endl;
+	
+	
+}
