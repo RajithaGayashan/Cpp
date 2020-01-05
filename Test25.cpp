3a3e3e5280b2b08880b9af463a5184800c1993cd
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-05 12:05:48 +0530

    Header file

---
+++ Test25.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "Tom.h"
+using namespace std;
+int main(){
+	Tom tom1;
+	tom1.eat();
+	tom1.run();
+	return 0;
+}
---
+++ Tom.h
@@ -0,0 +1,9 @@
+#ifndef TOM_H_
+#define TOM_H_
+class Tom{
+	public:
+		void run();
+		void eat();
+		
+	};
+#endif
