0b37b9790fae8d004220f499638023dd09b9bedf
RajithaGayashan <rajithagayashan95@gmail.com>
2020-03-04 23:42:44 +0530

    upcasting and downcasting

---
+++ Test41.cpp
@@ -0,0 +1,30 @@
+//Upcasting and downcasting
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    void print() { cout << "base" << endl; }
+};
+
+class Derived :public Base{
+public:
+    void print() {cout <<"derived" << endl;}
+};
+
+int main()
+{
+    // Upcasting
+    Base pBase;
+    Derived d;
+    //d.print();
+    pBase=d;
+    d.print();
+    
+
+    // Downcasting
+    Derived *pDerived;
+    Base *b;
+    pDerived = (Derived*)b;
+
+}
